8053bca0b9d01787bb701c09cb3e29f7d21d9ee2
johnkdunyo <johnkdunyo@gmail.com>
2023-04-05 16:56:01 +0000

    code cleanup

--- src/components/homePage/YouCanCountOnUs.tsx
+++ src/components/homePage/YouCanCountOnUs.tsx
@@ -32,7 +32,7 @@ const WhyChooseUsComponent = ({
 const YouCanCountOnUs = () => {
   return (
     <section className=" flex flex-col   pt-20 -mt-10 pb-8 relative ">
-      <div className="absolute  left-0 top-0 right-0 flex flex-col  justify-between  overflow-clip  h-full border">
+      <div className="absolute  left-0 top-0 right-0 flex flex-col  justify-between  overflow-clip  h-full ">
         {/* <div className=" hidden sm:block border-white h-[40rem] w-[40rem]  circle circle-1 shadow-2xl blur-md"></div> */}
         <div className=" border-red-400 h-[25rem] sm:h-[80rem] w-full sm:w-[80rem] top-[20%] sm:right-[20%]  circle circle-2"></div>
         <div className=" border-green-400 h-[25rem] sm:h-[60rem] w-full sm:w-[60rem]  circle circle-3"></div>
