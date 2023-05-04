eedf4befb91d7f3eeeddb3350570ae69ca37b938
johnkdunyo <johnkdunyo@gmail.com>
2023-05-04 16:11:19 +0000

    minor updates

--- src/components/spectrumFibrePage/RollingTextSection.tsx
+++ src/components/spectrumFibrePage/RollingTextSection.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const RollingTextSection = () => {
   return (
     <section className=" bg-white py-4 ">
-      <div className="bar_content flex justify-between gap-24 text-[#234222] font-bold text-3xl w-screen whitespace-nowrap overflow-clip">
+      <div className="bar_content flex justify-between gap-24 text-[#234222] font-bold text-3xl w-screen whitespace-nowrap ">
         <div className="  whitespace-nowrap">4000+km of Fibre optic cable</div>
         <div className="  whitespace-nowrap">4000+km of Fibre optic cable</div>
         <div className="  whitespace-nowrap">4000+km of Fibre optic cable</div>
