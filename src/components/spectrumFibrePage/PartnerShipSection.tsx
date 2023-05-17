f2bfdb9b7b86103d102dc50cce23a6a937dd0bc1
johnkdunyo <johnkdunyo@gmail.com>
2023-05-17 20:16:19 +0000

    fixed overlapping text

--- src/components/spectrumFibrePage/PartnerShipSection.tsx
+++ src/components/spectrumFibrePage/PartnerShipSection.tsx
@@ -3,10 +3,10 @@ import { Animate } from "../animations/ScrollAnimator";
 
 const PartnerShipSection = () => {
   return (
-    <section className="flex flex-col justify-between">
+    <section className="flex flex-col justify-between ">
       <div className="w-fulll bg-gradient-to-r from-[#17449A] to-[#0071BD] h-[30rem] md:h-[40rem]">
         <div className="md:hidden">
-          <div className=" custom-container mt-14">
+          <div className=" custom-container mt-14 ">
             <Animate.FadeUp className="  text-center paragraphText1 text-white ">
               Spectrum Fibre was formed out of a Public Private Partnership
               between Broadspectrum Limited and the Government of Ghana through
@@ -15,8 +15,8 @@ const PartnerShipSection = () => {
           </div>
           <img src="/assets/img/spectrum/sfc-collab.png" className="" />
         </div>
-        <div className=" hidden md:flex w-full   h-full justify-center items-center relative">
-          <Animate.FadeUp className="  text-center paragraphText1 text-white max-w-lg">
+        <div className=" hidden md:flex w-full   h-full justify-center items-center relative ">
+          <Animate.FadeUp className="  text-center paragraphText1 text-white max-w-sm ">
             Spectrum Fibre was formed out of a Public Private Partnership
             between Broadspectrum Limited and the Government of Ghana through
             the Ghana Infrastructure Investment Fund (GIIF) in 2017.
