943dd977d8160f45e217dcf45cd0ab4cb9bdbc26
johnkdunyo <johnkdunyo@gmail.com>
2023-05-24 08:53:24 +0000

    text update

--- src/components/infraServices/ReliableSection.tsx
+++ src/components/infraServices/ReliableSection.tsx
@@ -13,7 +13,7 @@ const ReliableSection = () => {
         </div>
         <Animate.FadeUp className="w-full h-full flex flex-col  justify-center gap-8  mb-16 custom-container  md:px-16  py-10">
           <h1 className="text-[#82AEC9] text-4xl md:text-[3.6rem] leading-normal md:leading-[1.2] font-bold">
-            Our top priority is creating reliable and effective communications
+            Our top priority is creating reliable and effective communication
             networks.
           </h1>
           <div className="text-xl leading-[1.5] ">
