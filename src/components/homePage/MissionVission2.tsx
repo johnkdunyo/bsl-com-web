93b6858c9da80a57ef5c1b3484c199c83ce8a0bc
johnkdunyo <johnkdunyo@gmail.com>
2023-07-05 10:30:50 +0000

    updated mission nd vission

--- src/components/homePage/MissionVission2.tsx
+++ src/components/homePage/MissionVission2.tsx
@@ -85,7 +85,7 @@ const MissionVission = () => {
         <div className=" sm:-ml-[30%] ">
           <h1 className="font-bold  md:text-[14rem] text-[#AB2346]">Mission</h1>
           <p className="paragraphText1 text-2xl">
-            To be Africa{"'"}s communication and <br /> digital partner of
+            To be the leading communication and <br /> digital partner of
             choice.
           </p>
         </div>
@@ -99,7 +99,7 @@ const MissionVission = () => {
           <h1 className="font-bold  md:text-[14rem]  text-[#AB2346]">Vision</h1>
           <p className="paragraphText1 px-4 sm:px-0 text-2xl">
             To be a world class communication and Digital <br />
-            partner of choice from Africa by Africans.
+            partner of choice.
           </p>
         </div>
       </section>
